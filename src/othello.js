de11d8e6e17a8012390dde3307ec37ca65275b58
shinoda <tetora1053@gmail.com>
2019-07-12 00:47:16 +0900

    render othello board

--- src/othello.js
+++ src/othello.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-export default class Board extends React.Component {
+export default class Game extends React.Component {
   render() {
     return (
-	  <div>board</div>
+      <Board />
     );
   }
 }
 
+class Board extends React.Component {
+  render() {
+    let squareRows = [];
+    for (let i = 0; i < 8; i++) {
+      squareRows.push(<SquareRow key={i} />);
+    }
+    return (
+      <div>
+        {squareRows}
+      </div>
+    );
+  }
+}
+
+class SquareRow extends React.Component {
+  render() {
+    let squares = [];
+    for (let i = 0; i < 8; i++) {
+      squares.push(<Square key={i} />);
+    }
+    return (
+      <div>{squares}</div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button></button>
+    );
+  }
+}
