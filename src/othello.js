da174efd92c6f41beaa5a64ede1e1bfc4f46caf8
shinoda <tetora1053@gmail.com>
2019-07-13 10:31:27 +0900

    implement the logic of reverseStone method.

--- src/othello.js
+++ src/othello.js
@@ -25,7 +25,6 @@ export default class Game extends React.Component {
   }
 
   handleSquareClick(e) {
-    console.log(e.target.value);
     const square_number = e.target.value;
     let square_states = this.state.squareStates;
     if (square_states[square_number] !== '') {
@@ -34,7 +33,13 @@ export default class Game extends React.Component {
     }
     const current_turn = this.state.turn;
 
-    const res = this.reverseStone(square_number, current_turn, 'left', true);
+    let res = false;
+    const direction_arr = ["left", "right", "up", "down", "up-left", "up-right", "down-left", "down-right"];
+    for (let i = 0; i < direction_arr.length; i++) {
+      if (this.reverseStone(Number(square_number), current_turn, direction_arr[i], true)) {
+        res = true;
+      }
+    }
     if (!res) {
       console.log("you cant put stone here.");
       return;
@@ -49,20 +54,86 @@ export default class Game extends React.Component {
   }
 
   reverseStone(square_number, current_turn, direction, is_base = true, limit_square_number = null) {
-    if (square_number < limit_square_number) {
-      return false;
+    if (!is_base) {
+      switch (true) {
+        case direction === 'left' && square_number < limit_square_number:
+        case direction === 'right' && square_number > limit_square_number:
+        case direction === 'up' && square_number < limit_square_number:
+        case direction === 'down' && square_number > limit_square_number:
+        case direction === 'up-left' && square_number < limit_square_number:
+        case direction === 'up-right' && square_number < limit_square_number:
+        case direction === 'down-left' && square_number > limit_square_number:
+        case direction === 'down-right' && square_number > limit_square_number:
+          return false;
+      }
     }
     if (is_base) {
-      limit_square_number = square_number - (square_number % 8);
+      switch (direction) {
+        case 'left':
+          limit_square_number = square_number - (square_number % 8);
+          break;
+        case 'right':
+          limit_square_number = square_number + (8 - (square_number % 8)) - 1;
+          break;
+        case 'up':
+          limit_square_number = square_number % 8;
+          break;
+        case 'down':
+          limit_square_number = 64 - (8 - (square_number % 8));
+          break;
+        case 'up-left':
+          limit_square_number = square_number - (Math.floor(square_number / 9) * 9);
+          break;
+        case 'up-right':
+          limit_square_number = square_number - (Math.floor(square_number / 7) * 7);
+          break;
+        case 'down-left':
+          limit_square_number = square_number + (Math.floor((63 - square_number) / 7) * 7)
+          break;
+        case 'down-right':
+          limit_square_number = square_number + (Math.floor((63 - square_number) / 9) * 9)
+          break;
+      }
     }
-    const next_square_state = this.state.squareStates[square_number - 1];
-    if (next_square_state === '') {
+    let next_square_number;
+    switch (direction) {
+      case 'left':
+        next_square_number = square_number - 1;
+        break;
+      case 'right':
+        next_square_number = square_number + 1;
+        break;
+      case 'up':
+        next_square_number = square_number - 8;
+        break;
+      case 'down':
+        next_square_number = square_number + 8;
+        break;
+      case 'up-left':
+        next_square_number = square_number - 9;
+        console.log("upleft next", next_square_number);
+        break;
+      case 'up-right':
+        next_square_number = square_number - 7;
+        break;
+      case 'down-left':
+        next_square_number = square_number + 7;
+        break;
+      case 'down-right':
+        next_square_number = square_number + 9;
+        break;
+    }
+    const next_square_state = this.state.squareStates[next_square_number];
+    if (next_square_number == 44) {
+      console.log("upleft next state", next_square_state);
+    }
+    if (next_square_state == undefined || next_square_state == '') {
       return false;
     }
     if ((is_base && next_square_state !== current_turn) || (!is_base && next_square_state !== current_turn)) {
-      if (this.reverseStone(square_number - 1, current_turn, direction, false, limit_square_number)) {
+      if (this.reverseStone(Number(next_square_number), current_turn, direction, false, limit_square_number)) {
         let square_states = this.state.squareStates;
-        square_states[square_number - 1] = current_turn;
+        square_states[next_square_number] = current_turn;
         this.setState({squareStates: square_states});
         return true;
       }
